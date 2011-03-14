ee912383067d31e9a0106c795e5f1c6c61d8532b
Anton Bukov <k06aaa@gmail.com>
2011-03-14 11:39:18 +0300

    Убрал чушь с порядком байтов однобайтовых типов.

--- DiplomBukov/i64u64.h
+++ DiplomBukov/i64u64.h
@@ -17,24 +17,20 @@ typedef unsigned __int64 u64;
 
 // Little Endian
 
-typedef i8   i8le;
 typedef i16 i16le;
 typedef i32 i32le;
 typedef i64 i64le;
 
-typedef u8   u8le;
 typedef u16 u16le;
 typedef u32 u32le;
 typedef u64 u64le;
 
 // Big Endian
 
-typedef boolib::util::BigEndian<i8>   i8be;
 typedef boolib::util::BigEndian<i16> i16be;
 typedef boolib::util::BigEndian<i32> i32be;
 typedef boolib::util::BigEndian<i64> i64be;
 
-typedef boolib::util::BigEndian<u8>   u8be;
 typedef boolib::util::BigEndian<u16> u16be;
 typedef boolib::util::BigEndian<u32> u32be;
 typedef boolib::util::BigEndian<u64> u64be;
@@ -43,24 +39,20 @@ typedef boolib::util::BigEndian<u64> u64be;
 
 // Little Endian
 
-typedef boolib::util::LittleEndian<i8>   i8le;
 typedef boolib::util::LittleEndian<i16> i16le;
 typedef boolib::util::LittleEndian<i32> i32le;
 typedef boolib::util::LittleEndian<i64> i64le;
 
-typedef boolib::util::LittleEndian<u8>   u8le;
 typedef boolib::util::LittleEndian<u16> u16le;
 typedef boolib::util::LittleEndian<u32> u32le;
 typedef boolib::util::LittleEndian<u64> u64le;
 
 // Big Endian
 
-typedef i8   i8be;
 typedef i16 i16be;
 typedef i32 i32be;
 typedef i64 i64be;
 
-typedef u8   u8be;
 typedef u16 u16be;
 typedef u32 u32be;
 typedef u64 u64be;
